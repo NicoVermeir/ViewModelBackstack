ad0d27c6fa1e0923da512f6d2d753111c5477c62
NicoVermeir <nico_vermeir@hotmail.com>
2013-10-17 11:12:07 +0200

    added sample project

---
+++ SampleApplication/GuidPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using Microsoft.Phone.Controls;
+using SampleApplication.ViewModel;
+using ViewModelBackstack;
+
+namespace SampleApplication
+{
+    public partial class GuidPage : PhoneApplicationPage
+    {
+        public GuidPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBackKeyPress(CancelEventArgs e)
+        {
+            if (ViewModelBackStack.CanGoBack())
+            {
+                DataContext = ViewModelBackStack.GoBack<GuidViewModel>();
+                e.Cancel = true;
+                return;
+            }
+
+            base.OnBackKeyPress(e);
+        }
+    }
+}
